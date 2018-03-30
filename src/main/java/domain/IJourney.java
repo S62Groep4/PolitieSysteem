371ddd41e638feb31faa7d91ba17e27992b73967
ThijsVries <t.vries@outlook.com>
2018-03-30 11:09:15 +0200

    Re-added domain classes, DAO's, rest layer and service layer

---
+++ src/main/java/domain/IJourney.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.util.List;
+
+
+public interface IJourney {
+    
+    List<ITransLocation> getTransLocations();
+
+}
---
+++ src/main/java/domain/ITransLocation.java
@@ -0,0 +1,15 @@
+package domain;
+
+
+public interface ITransLocation {
+    
+    Double getLat();
+    
+    Double getLon();
+    
+    String getDateTime();
+    
+    String getSerialNumber();
+    
+    String getCountryCode();
+}
